c000c1de9314f2a069f8fab5b9e39227086a3ead
Wendel dos Santos Nunes <wendelnunes9999@gmail.com>
2022-01-29 12:36:12 -0300

    Update project

--- public/scripts/cakesOrders.js
+++ public/scripts/cakesOrders.js
@@ -2,6 +2,6 @@ const imgNumber = Number(localStorage.getItem('img'))
 
 let main = document.querySelector('main div')
 
-let html = `<img src="./assets/bolo${imgNumber}.jpeg" alt="Fotos de bolos" />`
+let html = `<img src="/assets/bolo${imgNumber}.jpeg" alt="Fotos de bolos" />`
 
 main.insertAdjacentHTML('beforeend', html)
